dfa9db3bab57b87d4ccb1e470358857e1142ab10
Juan Builes <jbuilesgallo@gmail.com>
2023-07-28 00:06:06 -0500

    remove bloat

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -3,7 +3,7 @@ import { NextRequest } from 'next/server';
 
 export const runtime = 'edge';
 
-const interRegular = fetch(new URL('./Inter-Regular.ttf', import.meta.url)).then((res) =>
+const interRegular = fetch(new URL('./Inter-Bold.ttf', import.meta.url)).then((res) =>
   res.arrayBuffer()
 );
 
